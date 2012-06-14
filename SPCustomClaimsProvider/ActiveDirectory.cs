e2f208bf27195feda7f2fbbedbd21c4806cb0459
Jonathan Cooper <jonathan.cooper@pearson.com>
2012-06-14 12:09:37 +0100

    Now querying GC

--- SPCustomClaimsProvider/ActiveDirectory.cs
+++ SPCustomClaimsProvider/ActiveDirectory.cs
@@ -10,49 +10,21 @@ namespace SPCustomClaimsProvider
 
     public class ActiveDirectory
     {
-        private DirectorySearcher[] _directorySearchers;
-        private string[] _domains = {"mctest.local"};
-
-        public ActiveDirectory()
-        {
-            _directorySearchers = GetDirectorySearchers();
-        }
-        private DirectorySearcher[] GetDirectorySearchers()
-        {
-            var searchers = new List<DirectorySearcher>();
-
-            foreach (var userDomain in _domains)
-            {
-                var entry = new DirectoryEntry();
-                entry.Path = "LDAP://" + GetDomainDnName(userDomain); // This used to be the exchange user domain
-                entry.AuthenticationType = AuthenticationTypes.Secure;
-
-                var searcher = new DirectorySearcher(entry) { SearchRoot = entry };
-
-                searchers.Add(searcher);
-            }
-
-            return searchers.ToArray();
-        }
-
-        private string GetDomainDnName(string userDomain)
-        {
-            var context = new DirectoryContext(DirectoryContextType.Domain, userDomain);
-            var domain = Domain.GetDomain(context);
-            var directoryEntry = domain.GetDirectoryEntry();
-            return directoryEntry.Properties["DistinguishedName"].Value.ToString();
-        }
-
         public List<string> FindUsers(string searchCriteria)
         {
+            var entry = new DirectoryEntry();
+            entry.Path = "GC://" + "ukstr-ad-640.peroot.com"; // This used to be the exchange user domain
+            entry.AuthenticationType = AuthenticationTypes.Secure;
+
+            var searcher = new DirectorySearcher(entry) { SearchRoot = entry };
             List<string> returnSearch = new List<string>();
             searchCriteria = "*" + searchCriteria + "*";
-            foreach (var searcher in _directorySearchers)
-            {
-                searcher.Filter = "(&(objectClass=user)(|(givenName=" + searchCriteria + ")(surname=" + searchCriteria + "))";
-                var results = searcher.FindAll();
-                returnSearch.AddRange(from SearchResult result in results select result.GetDirectoryEntry().Name);
-            }
+
+
+            searcher.Filter = "(&(objectClass=User)(|(givenName=" + searchCriteria + ")(sn=" + searchCriteria + ")))";
+            var results = searcher.FindAll();
+            returnSearch.AddRange(from SearchResult result in results select result.GetDirectoryEntry().Name);
+
             return returnSearch;
         }
 
